1bdb640a90b39ff33c64a5076921a457c453629f
anton-mp <abramova2712katya@gmail.com>
2026-05-27 13:39:40 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,41 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
 #include "train.h"
+#include <random>
+
+void expAllOn(int n) {
+    Train train;
+    while (n--)
+        train.addCar(true);
+    train.getLength();
+    std::cout << train.getOpCount() << " ";
+}
+
+void expAllOff(int n) {
+    Train train;
+    while (n--)
+        train.addCar(false);
+    train.getLength();
+    std::cout << train.getOpCount() << " ";
+}
+
+void expAllRandom(int n) {
+    Train train;
+    srand(time(NULL));
+    while (n--)
+        train.addCar(rand() % 2);
+    train.getLength();
+    std::cout << train.getOpCount() << " ";
+}
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
-
-  while (count--)
-    train.addCar(false);
-
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    std::cout << "On | Off | Random" << std::endl;
+    for (int count = 2; count < 100; count++) {
+        std::cout << count << ": ";
+        expAllOn(count);
+        expAllOff(count);
+        expAllRandom(count);
+        std::cout << std::endl;
+    }
+    return 0;
 }
